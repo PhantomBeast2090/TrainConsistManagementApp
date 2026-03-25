1bc320402a28446e102f24d6ae002b7d5cde4824
Rakshan_Adithyaa <rakshanadithyaa@gmail.com>
2026-03-25 09:25:47 +0530

    Switch to HashMap for bogie management
    Replaced List of bogies with a HashMap to manage bogie types and counts.

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -5,9 +5,12 @@ public static void main(String args[]){
 
 System.out.println("=== Train Consist Management App ===");
 
-List<String> bogies=new ArrayList<>();
+HashMap<String,Integer> m=new HashMap<>();
 
-System.out.println("initial bogie count: "+bogies.size());
+m.put("Sleeper",72);m.put("AC Chair",60);m.put("First Class",40);
+
+for(Map.Entry<String,Integer> e:m.entrySet())
+System.out.println(e.getKey()+" -> "+e.getValue());
 
 }
 }
