f9ab4b220ad7183549b849b413e9effec1846649
Rakshan_Adithyaa <rakshanadithyaa@gmail.com>
2026-03-25 09:27:22 +0530

    Refactor bogie management to use Bogie class
    Replaced List<String> with List<Bogie> to manage bogies as objects. Added Bogie class and sorted the list based on capacity.

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,13 +1,22 @@
 import java.util.*;
 
+class Bogie{
+String name;int cap;
+Bogie(String n,int c){name=n;cap=c;}
+}
+
 class TrainApp{
 public static void main(String args[]){
 
 System.out.println("=== Train Consist Management App ===");
 
-List<String> bogies=new ArrayList<>();
+List<Bogie> l=new ArrayList<>();
 
-System.out.println("initial bogie count: "+bogies.size());
+l.add(new Bogie("Sleeper",72));l.add(new Bogie("AC Chair",56));l.add(new Bogie("First Class",40));
+
+Collections.sort(l,(a,b)->a.cap-b.cap);
+
+for(Bogie x:l)System.out.println(x.name+" "+x.cap);
 
 }
 }
