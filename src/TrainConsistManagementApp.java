66a9715698aaa9655d2a5403afdc32415dcf46ae
Rakshan_Adithyaa <rakshanadithyaa@gmail.com>
2026-03-25 09:12:58 +0530

    Refactor bogie management in TrainConsistManagementApp

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -5,9 +5,17 @@ public static void main(String args[]){
 
 System.out.println("=== Train Consist Management App ===");
 
-List<String> bogies=new ArrayList<>();
+ArrayList<String> b=new ArrayList<>();
 
-System.out.println("initial bogie count: "+bogies.size());
+b.add("Sleeper");b.add("AC Chair");b.add("First Class");
+
+System.out.println("after adding: "+b);
+
+b.remove("AC Chair");
+
+System.out.println("after removing: "+b);
+
+System.out.println("sleeper exists? "+b.contains("Sleeper"));
 
 }
 }
