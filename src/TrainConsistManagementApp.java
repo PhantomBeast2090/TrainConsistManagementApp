d5de47f7472c8517275dd0d4fd42ef2a94088fee
Rakshan_Adithyaa <rakshanadithyaa@gmail.com>
2026-03-25 09:11:31 +0530

    Add initial setup for Train Consist Management App

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,1 +1,13 @@
+import java.util.*;
 
+class TrainApp{
+public static void main(String args[]){
+
+System.out.println("=== Train Consist Management App ===");
+
+List<String> bogies=new ArrayList<>();
+
+System.out.println("initial bogie count: "+bogies.size());
+
+}
+}
